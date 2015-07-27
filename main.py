51cfda32882399c2097e94e7448685bd9088b81b
Eduardo Padoan <eduardo@d3.do>
2015-07-27 16:34:03 -0300

    history and syntax highlight

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 from prompt_toolkit.shortcuts import get_input
+from prompt_toolkit.history import History
+from pygments.lexers import IokeLexer
 
 def main():
-    text = get_input("> ")
-    print('You entered:', text)
+    history = History()
+    while True:
+        try:
+            text = get_input("> ", lexer=IokeLexer, history=history)
+        except Exception:
+            break
+        else:
+            print('You entered:', text)
+    print("Goodbye")
 
 if __name__ == '__main__':
     main()
