30aab9a498253e32a8b4208128f7c6ea1d365999
Eduardo Padoan <eduardo@d3.do>
2015-07-27 19:03:23 -0300

    Actually run expression on ioke

--- main.py
+++ main.py
@@ -2,29 +2,44 @@
 
 from __future__ import unicode_literals
 from __future__ import print_function
+
+import pexpect
+
 from prompt_toolkit.shortcuts import get_input
 from prompt_toolkit.history import History
 from prompt_toolkit.contrib.completers import WordCompleter
 from pygments.lexers import IokeLexer
 
+IOKE = '/usr/bin/env ioke'
 IOKE_VOCAB  = ['method'] # TODO
+IOKE_PROMPT = 'iik> '
+IOKE_DEBUG = 'dbg:1> '
+#IOKE_RESULT = '\+> .*'
 
 ioke_completer = WordCompleter(IOKE_VOCAB)
 
 def main():
+    ioke = pexpect.spawnu(IOKE)
     history = History()
     line_num = 1
+    i = None
     while True:
         try:
             text = get_input("{} > ".format(line_num), lexer=IokeLexer,
                 history=history, completer=ioke_completer)
+            if i is None:
+                i = ioke.expect([IOKE_PROMPT, IOKE_DEBUG])
         except EOFError:
             break
         except KeyboardInterrupt:
             text = ""
         else:
+            # TODO: diferentiate prompt (i = 0) and debug (i = 1)
             if text:
-                print('=> ', text)
+                ioke.sendline(text)
+                i = ioke.expect([IOKE_PROMPT, IOKE_DEBUG])
+                print('echo> ', ioke.before)
+                # TODO: Parse prints and +> (result)
                 line_num += 1
     print("Goodbye")
 
